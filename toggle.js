d520d6c1dbd9fed4ea25b983f22af289764cacc8
richardigarcia <160411446+richardigarcia@users.noreply.github.com>
2024-02-21 01:42:42 -0400

    Add files via upload

---
+++ toggle.js
@@ -0,0 +1,14 @@
+var toggle = document.getElementById('container');
+var body = document.querySelector('body');
+
+toggle.onclick = function(){
+    toggle.classList.toggle('active');
+    body.classList.toggle('active');
+
+
+}
+
+// let a =prompt('Cual es tu nombre?');
+// alert(`hola ${a}`);
+// alert("Bienvenido a PUMFEEM");
+
